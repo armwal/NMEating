b2fb3024b8595ff8edf7797e42bd98e8762b22ed
armwal <walterarmin@googlemailcom>
2019-01-12 23:50:07 +0100

    Added parser for TTR

---
+++ NMEating/NMIParser.py
@@ -0,0 +1,75 @@
+import re
+import datetime
+
+from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import PDFPageAggregator
+from pdfminer.layout import LAParams, LTTextBox, LTTextLine
+
+def process_file(filename):
+    text = extract_text_from_pdf(filename)
+    meals = parse_meal_text(text)
+    date = parse_date(text)
+    return meals,date
+
+def parse_cost_line(line):
+    money = re.compile('[0-9]+,[0-9][0-9] EUR')
+    found = money.findall(line)
+    return found
+
+def parse_meal_text(text):
+    daysOfWeek = ['Montag','Dienstag','Mittwoch','Donnerstag','Freitag']
+    currentDay = 0
+    lines = text.splitlines()
+    meals = []
+    for i in range(len(lines)):
+        line = lines[i]
+        if line.strip() == daysOfWeek[currentDay]:
+            currentDay += 1
+            meal = lines[i+1].strip().strip('1234567890,')
+            cost = lines[i+2].strip()
+            if any(char.isdigit() for char in cost):
+                # probably a meal
+                costs = parse_cost_line(cost)
+                meals = meals + [tuple([meal, costs])]
+            elif any(char.isdigit() for char in lines[i+3].strip()):
+                # probably two-line meal
+                meal += ' ' + cost.strip('1234567890,')
+                cost = lines[i+3].strip()
+                costs = parse_cost_line(cost)
+                meals = meals + [tuple([meal, costs])]
+            if currentDay == len(daysOfWeek):
+                break
+
+    return meals
+
+def parse_date(text):
+    lines = text.splitlines()
+    date = re.compile(r'.*([0-3][0-9]\.[0-1][0-9]\.20[0-9][0-9])')
+    for line in lines:
+        if 'Woche vom' in line:
+            match = date.match(line)
+            if match:
+                return datetime.datetime.strptime(match.group(1),'%d.%m.%Y')
+
+
+def extract_text_from_pdf(filename):
+    fp = open(filename, 'rb')
+    parser = PDFParser(fp)
+    doc = PDFDocument()
+    parser.set_document(doc)
+    doc.set_parser(parser)
+    doc.initialize('')
+    rsrcmgr = PDFResourceManager()
+    laparams = LAParams()
+    device = PDFPageAggregator(rsrcmgr, laparams=laparams)
+    interpreter = PDFPageInterpreter(rsrcmgr, device)
+    text = ""
+    # Process each page contained in the document.
+    for page in doc.get_pages():
+        interpreter.process_page(page)
+        layout = device.get_result()
+        for lt_obj in layout:
+            if isinstance(lt_obj, LTTextBox) or isinstance(lt_obj, LTTextLine):
+                text += lt_obj.get_text()
+    return text
---
+++ NMEating/TTRParser.py
@@ -0,0 +1,83 @@
+import os
+import subprocess
+import re
+from PIL import Image
+
+def process_file(filePath, outFolder):
+    tess = r'c:\Program Files (x86)\Tesseract-OCR\tesseract.exe'
+    image = Image.open(filePath)
+    meals = []
+    for day in range(5):
+        lines = process_sub_image(image, tess, outFolder, filePath, day)
+        soup, meal, mealcost, veg, vegcost, dessert = parse_lines(lines)
+        meals = meals + [tuple([soup, meal, mealcost, veg, vegcost, dessert])]
+    return meals
+
+
+def process_sub_image(image, tess, outFolder, filePath, day):
+    fileName = os.path.basename(filePath)
+    baseFileName,ext = fileName.split('.')
+    width = 210
+    left = 154 + day*width
+    top = 233
+    bottom = 650
+    cropped = image.crop((left, top, left+width, bottom))
+    outfilename = outFolder + baseFileName + '_' + str(day) + '.' + ext
+    cropped.save(outfilename)
+    tessname = outFolder + baseFileName + '_' + str(day)
+    subprocess.run([tess, outfilename, tessname, '-l', 'deu'])
+    f = open(tessname + '.txt', encoding='utf8')
+    lines = f.readlines()
+    f.close()
+    os.remove(outfilename)
+    os.remove(tessname + '.txt')
+    return list([l.strip() for l in lines])
+    
+def parse_lines(lines):
+    print(lines)
+    i = 0
+    while len(lines[i]) == 0:
+        i += 1
+    soup = lines[i]
+    i += 1
+    while len(lines[i]) == 0:
+        i += 1
+    meal = ''
+    while len(lines[i]) > 0:
+        meal += ' ' + lines[i]
+        i += 1
+    while len(lines[i]) == 0:
+        i += 1
+    if any(char.isdigit() for char in lines[i]):
+        mealcost = lines[i]
+        if '.' in mealcost:
+            mealcost = mealcost.replace('.',',')
+        elif ',' not in mealcost:
+            mealcost = mealcost[0] + ',' + mealcost[1:]
+        i += 1
+        while len(lines[i]) == 0:
+            i += 1
+        veg = ''
+    else:
+        mealcost = '?'
+        veg = lines[i]
+        i += 1
+    while len(lines[i]) > 0:
+        veg += ' ' + lines[i]
+        i += 1
+    while len(lines[i]) == 0:
+        i += 1
+    if any(char.isdigit() for char in lines[i]):
+        vegcost = lines[i]
+        if '.' in vegcost:
+            vegcost = vegcost.replace('.',',')
+        elif not ',' in vegcost:
+            vegcost = vegcost[0] + ',' + vegcost[1:]
+        i += 1
+        while len(lines[i]) == 0:
+            i += 1
+        dessert = lines[i]
+    else:
+        vegcost = '?'
+        dessert = lines[i]
+    return soup, meal, mealcost, veg, vegcost, dessert
